05375ba72f233e78956d9504f5ca101a5274a40a
Jose <josefbautista94@gmail.com>
2025-04-24 00:53:48 -0400

    Added GET route to retrieve all users from the database

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,19 @@
 import express from "express"
 import User from '../models/User.js'
 
-const router = express.Router();
+const router = express.Router(); // Create a mini Express app to handle routes for this feature
+
+//GET all users 
+router.get('/', async (req, res) => { // Defines a GET route for /users, async means you can use await inside this function to handle asynchronous tasks (like database calls).
+
+    try {
+        const users = await User.find(); // This fetches all users from the users collection in MongoDB, since it’s asynchronous, you use await to wait for it to complete.
+        res.json(users); //Sends the array of users back as a JSON response.
+
+
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message }); // If there’s any error during the process it sends back a 500 Internal Server Error with a message
+    }
+
+})
