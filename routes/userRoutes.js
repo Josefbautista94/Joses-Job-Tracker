ba141edbaf5bf203745e7ea086e4e9c3c3f79ec8
Jose <josefbautista94@gmail.com>
2025-04-24 10:55:05 -0400

    Added POST route to create a new user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,4 +16,20 @@ router.get('/', async (req, res) => { // Defines a GET route for /users, async m
         res.status(500).json({ message: err.message }); // If there’s any error during the process it sends back a 500 Internal Server Error with a message
     }
 
+});
+
+//POST new user
+router.post('/', async (req, res) => { // Defining a POST route, when someone sends a POST request to /users, this function runs, async allows you to use await inside of the function for async tasks like saving to the DB
+    try {
+        const { name, email } = req.body; // Using destructuring to pull name and email from the body of the request
+        const newUser = new User({ name, email }); // Creating a new instance of the User model using the provided name and email
+        await newUser.save() // Saves the new user to mongoDB
+        res.status(201).json(newUser);  //Successfully created
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message }); //If there’s an error (like missing fields, duplicate email, etc.), this code runs.
+
+
+    }
+
 })
