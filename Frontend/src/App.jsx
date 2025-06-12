d8e9763ca5fe2c8b59d9d5cf98828ca76c7b0893
Jose <josefbautista94@gmail.com>
2025-06-12 03:39:56 -0400

    Implement role-based NavBar with login/logout visibility and admin-only links

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import NavBar from '../components/NavBar';
+import NavBar from './components/NavBar';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
